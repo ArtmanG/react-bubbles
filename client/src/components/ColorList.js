bf3a57b5d269c46894d3f60f4baa670b246f37ed
Gregory Hawman <g.hawman@gmail.com>
2020-03-27 10:35:03 -0700

    can add colors now

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -11,6 +11,7 @@ const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  const [newColor, setNewColor] = useState(initialColor);
 
   const history = useHistory();
 
@@ -36,10 +37,19 @@ const ColorList = ({ colors, updateColors }) => {
       .then(res => {
         updateColors(colors.filter(color => color.id !== res.data));
         history.push('/bubble-page');
-      });
+      })
+      .catch(err => console.log(err));
   };
- 
-  
+
+  const addColor = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post('/api/colors', newColor)
+      .then(res => {
+        updateColors(res.data)
+      })
+      .catch(err => console.log(err));
+  };
 
   return (
     <div className="colors-wrap">
@@ -94,22 +104,38 @@ const ColorList = ({ colors, updateColors }) => {
           </div>
         </form>
       )}
-      <div className="spacer" />
+      
       {/* stretch - build another form here to add a color */}
+      <form onSubmit={addColor}>
+        <legend>add color</legend>
+        <label>
+          color name:
+          <input
+            onChange = {e => 
+              setNewColor({ ...newColor, color: e.target.value })
+            }
+            value={newColor.color}
+          />
+        </label>
+        <label>
+          hex code:
+          <input
+            onChange = {e => 
+              setNewColor({ 
+                ...newColor, 
+                code: {hex: e.target.value }
+              })
+            }
+            value={newColor.code.hex}
+          />
+        </label>
+        <div className="button-row">
+            <button type="submit">add</button>
+          </div>
+      </form>
+      <div className="spacer" />
     </div>
   );
 };
 
 export default ColorList;
-
-// Adds a color
-// const saveEdit = e => {
-//   e.preventDefault();
-//   axiosWithAuth()
-//     .put(`/api/colors/${colorToEdit.id}`, colorToEdit)
-//     .then(res => {
-//       updateColors([...colors, res.data]);
-//       history.push('/bubble-page');
-//     })
-//     .catch(err => console.log(err));
-// };
